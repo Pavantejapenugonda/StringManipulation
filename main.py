bdd465f460db2f21d4af5965db7f4c0caddbb01c
Pavanteja penugonda <55921043+Pavantejapenugonda@users.noreply.github.com>
2024-03-14 02:02:44 -0400

    added des readme and updated req.txt

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ import sys
 import re
 
 def change_the_directory(current_path, new_path):
+    """ This function change the directory path based on
+    new path provided
+    Args:
+        current_path (str): Current path of the directory
+        new_path (str): New path or commands to change the directory
+
+    Returns:
+        Modified path (str): Path generated based on new path
+    """
     # Avoid the duplicate /'s in the new path
     mod_new_path = re.sub(r'/{2,}', '/', new_path)
 
     # 1. If current path is "/"
+    # # Handling the scenario if new path contains only "/"
     if current_path == "/":
-        # Handling the scenario if new path contains starting "/"
         return current_path + mod_new_path.lstrip("/")
     
-    # 2. folder name wihout starting /
-    if re.match("[a-zA-Z0-9_]", mod_new_path):
-        return current_path + "/" + mod_new_path
-    
-    # 3. Handling mod_new_path is only '/'
+    # 2. Handling mod_new_path is only '/'
     if mod_new_path.startswith('/'):
         return mod_new_path
+    
+    # 3. folder name wihout starting /
+    # # Handling the scenario if we don't any /'s at starting
+    if re.match("[a-zA-Z0-9_]", mod_new_path):
+        return current_path + "/" + mod_new_path
 
     # Split the paths into components
     current_components = current_path.split('/')
@@ -25,7 +35,6 @@ def change_the_directory(current_path, new_path):
     # 4. if we have only "." in a string
     if "/" not in mod_new_path and '.' in mod_new_path:
         list_double_dots = re.findall(r'.{2}', mod_new_path)
-        print(list_double_dots)
         if len(list_double_dots) <= len(list(filter(None, current_components))):
             for ele in list_double_dots:
                 if ele == "..":
@@ -36,7 +45,7 @@ def change_the_directory(current_path, new_path):
         else:
             return f"{mod_new_path}: No such file or directory"
 
-    # Traverse the new path components
+    # 5. Handling all other special secnario's
     for component in new_components:
         if component == '.':
             continue
@@ -54,6 +63,5 @@ if __name__ == "__main__":
     
     current_directory = sys.argv[1]
     new_directory = sys.argv[2]
-
     result = change_the_directory(current_directory, new_directory)
-    print(result)
+    print(f"Generated result path : {result}")
